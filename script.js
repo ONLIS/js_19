2d3de1b885b13cb5c5caae2d97f53c21315c12bb
onlis <aleksei@onl.is>
2021-12-22 01:14:22 +0500

    lesson 19_3 done

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 "use strict";
+
+const img = document.querySelector("img");
+
+const button = document.querySelector("#startStopButton");
+const reset = document.querySelector("#resetButton");
+let idInterval;
+let count = 0;
+let forward = true;
+let active = false;
+
+const move = () => {
+  if (active) {
+    if (forward && count < 300) {
+      count++;
+      if (count == 170) {
+        forward = false;
+      }
+    } else if (!forward && count > 0) {
+      count--;
+      if (count == 0) {
+        forward = true;
+      }
+    }
+    requestAnimationFrame(move);
+    img.style.left = count * 5 + "px";
+  }
+};
+
+button.addEventListener("click", () => {
+  active = !active;
+  if (active) {
+    idInterval = requestAnimationFrame(move);
+  } else {
+    cancelAnimationFrame(idInterval);
+  }
+});
+reset.addEventListener("click", () => {
+  cancelAnimationFrame(idInterval);
+  active = false;
+  forward = true;
+  count = 0;
+  img.style.left = 0;
+});
